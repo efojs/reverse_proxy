1513f2f0299d232d3bfd11f2f8d53922a0913553
Denis <denis@efojs.com>
2021-11-27 01:28:27 +0300

    Update test fixtures

--- test_proxy/fixtures/fixtures.py
+++ test_proxy/fixtures/fixtures.py
@@ -1,15 +1,19 @@
 import pytest
 
+test_words = """\
+    Sprite™-ish Sprite-ish sprite Sprite™ sprite/Sprite™/sprite! \
+    600000 Straße пришёл spri_e sprite algúns التوهم"""
+
+test_words_result = """\
+    Sprite™-ish Sprite™-ish sprite™ Sprite™ sprite™/Sprite™/sprite™! \
+    600000 Straße™ пришёл™ spri_e sprite™ algúns™ التوهم™"""
+
 
 @pytest.fixture
 def get_test_source_body():
-    return """<a href="news">Hacker News</a><td>Im developing ... random
-    flow fields... <p>600000 ways always looking Friday/Cybermonday social
-    skills?</p></td><pre><code>[string in list(soup.strings)]</code></pre>"""
+    return """{0}<p>{0}</p><code>{0}</code>""".format(test_words)
 
 
 @pytest.fixture
 def get_test_result_body():
-    return """<a href="news">Hacker™ News</a><td>Im developing ... random™
-    flow fields™... <p>600000 ways always™ looking Friday™/Cybermonday social™
-    skills™?</p></td><pre><code>[string in list(soup.strings)]</code></pre>"""
+    return """{0}<p>{0}</p><code>{1}</code>""".format(test_words_result, test_words)
